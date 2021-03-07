f3207b7f034c01a8b6cd821a7afe0816f876e3dc
Alise Ponsero <alise@Alises-MacBook-Pro.local>
2021-03-07 18:18:22 +0200

    database

--- shiny_app/App.R
+++ shiny_app/App.R
@@ -7,45 +7,14 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(readr)
-
-
-# logifySlider javascript function
-JS.logify <-
-  "
-// function to logify a sliderInput
-function logifySlider (sliderId, sci = false) {
-  if (sci) {
-    // scientific style
-    $('#'+sliderId).data('ionRangeSlider').update({
-      'prettify': function (num) { return ('10<sup>'+num+'</sup>'); }
-    })
-  } else {
-    // regular number style
-    $('#'+sliderId).data('ionRangeSlider').update({
-      'prettify': function (num) { return (Math.pow(10, num)); }
-    })
-  }
-}"
-
-# call logifySlider for each relevant sliderInput
-JS.onload <-
-  "
-// execute upon document loading
-$(document).ready(function() {
-  // wait a few ms to allow other scripts to execute
-  setTimeout(function() {
-    // include call for each slider
-    logifySlider('DB_citation', sci = false)
-  }, 5)})
-"
-
-
+library(DT)
 
 # ----------------------------------
 # load dataset
-datasetJL <- read_csv("../datasets/JL_raw_2020.csv") 
+datasetJL <- read_csv("../datasets/JL_database_2018.csv") 
 datasetJL$article_year <- as.numeric(datasetJL$article_year)
 
+articlesJL <- read_csv("../datasets/JL_article_2018.csv") 
 
 ui <- navbarPage(title = "DS Heroes",
                  theme = "style/style.css",
@@ -71,29 +40,41 @@ ui <- navbarPage(title = "DS Heroes",
                  tabPanel(title = "DB JL",
                          sidebarLayout(fluid=TRUE,
                            sidebarPanel(h2("Selection databases"),
-                                        sliderInput("DB_year", "date of publication:",
+                                        sliderInput("DB_year", "date of first publication:",
                                                     min = 1991, max = 2020,
                                                     value = c(2010,2020)),
                                         sliderInput("DB_citation", "number of citations:",
-                                                    min = 0, max = 17000,
-                                                    value = c(0,17000)),
-                                        downloadButton("downloadData", "Download selection")
+                                                    min = 0, max = 18000,
+                                                    value = c(0,18000)),
+                                        sliderInput("nb_publi", "number of publications:",
+                                                    min = 0, max = 25,
+                                                    value = c(0,25)),
+                                        checkboxGroupInput("access", "Availability:",
+                                                           c("available" = "yes",
+                                                             "not available" = "no"),
+                                                           selected=c("yes", "no")),
+                                        h4(),
+                                        downloadButton("downloadData", "Download databases"),
+                                        h4(),
+                                        downloadButton("downloadArticles", "Download articles"),
                                         ),
                            
                            
                            mainPanel(width = 8,
                                      fluidRow(
-                                       column(6,plotOutput("avaibility_chart")),
-                                       column(6,plotOutput("nb_citations_chart")),
+                                       column(5,plotOutput("avaibility_chart")),
+                                       column(5,plotOutput("nb_citations_chart")),
                                      ),
-                                     fluidRow(column(12,tableOutput("tableview")))
+                                     fluidRow(column(10, 
+                                                     h2("List of selected ressources"),
+                                                     DT::dataTableOutput("tableview")))
                                     ) 
                          ),
                   ),
                 # ----------------------------------
                 # tab panel 3 - Code Av
                 tabPanel("Code Av",
-                         includeHTML("more_information.html"),
+                         includeHTML("home.html"),
                          tags$script(src = "plugins/scripts.js"),
                          tags$head(
                            tags$link(rel = "stylesheet", 
@@ -120,8 +101,15 @@ server <- function(input, output) {
   # ----------------------------------
   # tab panel 2-A - DB JL
     db_selection <- reactive({
-      datasetJL %>%  filter(article_year>=input$DB_year[1] & article_year<=input$DB_year[2]) %>%
-        filter(citations>=input$DB_citation[1] & citations<=input$DB_citation[2]) 
+      datasetJL %>%  filter(year_first_publication>=input$DB_year[1] & year_first_publication<=input$DB_year[2]) %>%
+        filter(total_citation>=input$DB_citation[1] & total_citation<=input$DB_citation[2]) %>%
+        filter(nb_articles>=input$nb_publi[1] & nb_articles<=input$nb_publi[2]) %>%
+        filter(available %in% input$access)
+    })
+    
+    article_selection <- reactive({
+      list<- db_selection() %>% select(db_id)
+      inner_join(articlesJL, list)
     })
     
     output$avaibility_chart <- renderPlot({  
@@ -130,31 +118,41 @@ server <- function(input, output) {
       display_dataset  %>%  ggplot(aes(x="", y=n, fill=available)) +
         geom_bar(stat="identity", width=1, color="white") +
         coord_polar("y", start=0) +
-        theme_void()
+        theme_void()+
+        ggtitle("Availability of ressources")
     })
     
     output$nb_citations_chart <- renderPlot({  
       display_dataset <- db_selection() 
       
-      display_dataset %>% ggplot(aes(x=citations)) +
-        geom_histogram(colour="black", fill="blue")
+      display_dataset %>% ggplot(aes(x=total_citation)) +
+        geom_histogram(fill="#00BFC4") +
+        theme_minimal()+
+        ggtitle("Distribution of citations")
     })
     
     
-    output$tableview <- renderTable({
-      db_selection() %>% select(article_global_id, resource_name, access)
-    }, digits = 0)
+    output$tableview <- renderDataTable({
+      db_selection() %>% select(db_id, resource_name, link_HTML)
+    }, escape = FALSE)
     
     # ----------------------------------
     # Download button
-    
-    
     output$downloadData <- downloadHandler(
       filename = function() {
         paste("DataJL-", Sys.Date(), ".csv", sep="")
       },
       content = function(file) {
         write.csv(db_selection(), file)
+      }
+    )
+    
+    output$downloadArticles <- downloadHandler(
+      filename = function() {
+        paste("ArticleJL-", Sys.Date(), ".csv", sep="")
+      },
+      content = function(file) {
+        write.csv(article_selection(), file)
       }
     )
   
